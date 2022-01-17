f3f95374ad363b32ee85caeed2f4d6fa41da0cc3
heimb <heim.bogdan@hotmail.com>
2022-01-17 12:28:06 +0200

    Add main command

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # go-enigma
 
-No main / executable function yet!
+```code
+Usage: main.go ACTION_TYPE ALPHABET_TYPE CIPHER_NUM_1 CIPHER_NUM_2 CIPHER_NUM_3 CIPHER_NUM_CHANGE MESSAGE
+
+ACTION_TYPE         - 'encode' or 'decode'
+ALPHABET_TYPE       - Integer that will represent the alphaber type used. Values: 0 - ALPHA , 1 - NUMERIC, 2 - ALPHA_NUMERIC, 3 - ALPHA_NUMERIC_SYMBOL
+CIPHER_NUM_1        - Chiper number 1
+CIPHER_NUM_2        - Chiper number 2
+CIPHER_NUM_3        - Chiper number 3
+CIPHER_NUM_CHANGE   - A number used as a counter. When reached the cipher is changed.
+MESSAGE             - Message to be ecoded / decoded
 
 This can be used as a library now, by importing `github.com/heimbogdan/go-enigma/cipher`
+```
 
 ## Usage
 
--- main.go
+++ main.go
@@ -2,18 +2,42 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"os"
+	"strconv"
 
 	"github.com/heimbogdan/go-enigma/cipher"
+	c "github.com/heimbogdan/go-enigma/internal/cipher"
 )
 
 func main() {
-	start := time.Now()
-	cipher.SetScramble(cipher.ALPHA_NUMERIC_SYMBOLS, 11, 25, 3, 3)
-	encMessage := cipher.Encode("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-	fmt.Println(encMessage)
-	fmt.Println(cipher.Decode(encMessage))
-	end := time.Now()
-	elapsed := end.Sub(start)
-	fmt.Printf("Total duration: %d microseconds", elapsed.Microseconds())
+	if len(os.Args) < 8 {
+		printHelp()
+	} else {
+		action := os.Args[1]
+		aType, _ := strconv.Atoi(os.Args[2])
+		c1, _ := strconv.Atoi(os.Args[3])
+		c2, _ := strconv.Atoi(os.Args[4])
+		c3, _ := strconv.Atoi(os.Args[5])
+		cc, _ := strconv.Atoi(os.Args[6])
+		cipher.SetScramble(c.AlphabetType(aType), c1, c2, c3, cc)
+		if action == "encode" {
+			fmt.Println(cipher.Encode(os.Args[7]))
+		} else if action == "decode" {
+			fmt.Println(cipher.Decode(os.Args[7]))
+		} else {
+			fmt.Println("Wrong ACTION_TYPE!")
+			printHelp()
+		}
+	}
+}
+
+func printHelp() {
+	fmt.Println("Usage: main.go ACTION_TYPE ALPHABET_TYPE CIPHER_NUM_1 CIPHER_NUM_2 CIPHER_NUM_3 CIPHER_NUM_CHANGE MESSAGE")
+	fmt.Println("\nACTION_TYPE         - 'encode' or 'decode'")
+	fmt.Println("ALPHABET_TYPE       - Integer that will represent the alphaber type used. Values: 0 - ALPHA , 1 - NUMERIC, 2 - ALPHA_NUMERIC, 3 - ALPHA_NUMERIC_SYMBOL")
+	fmt.Println("CIPHER_NUM_1        - Chiper number 1")
+	fmt.Println("CIPHER_NUM_2        - Chiper number 2")
+	fmt.Println("CIPHER_NUM_3        - Chiper number 3")
+	fmt.Println("CIPHER_NUM_CHANGE   - A number used as a counter. When reached the cipher is changed.")
+	fmt.Println("MESSAGE             - Message to be ecoded / decoded")
 }
